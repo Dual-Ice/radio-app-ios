29dd2d7d2e4a146ab31b77af7a40dd0785183c85
Maksim Stogniy <mstogniy@ya.ru>
2024-07-30 18:34:18 +0400

    Init empty folders

---
+++ radio-app-ios/Components/Components.swift
@@ -0,0 +1,10 @@
+//
+//  Components.swift
+//  radio-app-ios
+//
+//  Created by  Maksim Stogniy on 30.07.2024.
+//
+
+import Foundation
+
+// some code
---
+++ radio-app-ios/Screens/Detail/Detail.swift
@@ -0,0 +1,10 @@
+//
+//  Detail.swift
+//  radio-app-ios
+//
+//  Created by  Maksim Stogniy on 30.07.2024.
+//
+
+import Foundation
+
+// some code
