3aad53066a096e072b5da6c65faf05c5dcb912ec
Nikita Moskalev <nikmosyl@gmail.com>
2024-08-01 23:16:14 +0400

    add AudioPleer

--- radio-app-ios/Components/VerticalTabBar/VerticalTabBarPresenter.swift
+++ radio-app-ios/Components/VerticalTabBar/VerticalTabBarPresenter.swift
@@ -42,6 +42,15 @@ final class VerticalTabBarPresenter: VerticalTabBarPresenterProtocol {
 
 #warning("TO DO: Заглушки. Удалить после заполнения viewControllers[:]")
 class testVC1: UIViewController {
+    
+    let moks: [String] = [
+        "https://0n-jazz.radionetz.de/0n-jazz.mp3",
+        "https://stream.rockantenne.de/alternative/stream/mp3",
+        "https://stream.zeno.fm/fu24r3f24c9uv",
+        "https://stream-176.zeno.fm/a3th9bd1fs8uv?zs=e8ersugnTZy8yQsXXp2s2A",
+        "https://drive.uber.radio/uber/crberlinphilharmonic/icecast.audio"
+    ]
+    
     override func loadView() {
         let testView = UIView()
         testView.backgroundColor = .green
@@ -54,19 +63,47 @@ class testVC1: UIViewController {
         button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         
+        let playButton = UIButton()
+        playButton.setTitle("PLAY RANDOM", for: .normal)
+        playButton.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let pauseButton = UIButton()
+        pauseButton.setTitle("PAUSE", for: .normal)
+        pauseButton.addTarget(self, action: #selector(pauseButtonTapped), for: .touchUpInside)
+        pauseButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         testView.addSubview(label)
         testView.addSubview(button)
+        testView.addSubview(playButton)
+        testView.addSubview(pauseButton)
+        
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: testView.centerYAnchor),
             button.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
-            button.topAnchor.constraint(equalTo: label.bottomAnchor)
+            button.topAnchor.constraint(equalTo: label.bottomAnchor),
+            playButton.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
+            playButton.topAnchor.constraint(equalTo: button.bottomAnchor),
+            pauseButton.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
+            pauseButton.topAnchor.constraint(equalTo: playButton.bottomAnchor),
         ])
         view = testView
     }
     
     @objc private func buttonTapped(_ sender: CustomTabBarButton) {
         navigationController?.pushViewController(FinalVC(), animated: true)
+    }
+    
+    @objc private func playButtonTapped(_ sender: CustomTabBarButton) {
+        let url = moks.randomElement()
+        AudioPleer.shared.loadMusic(from: url ?? "")
+        AudioPleer.shared.playMusic()
+    }
+    
+    @objc private func pauseButtonTapped(_ sender: CustomTabBarButton) {
+        AudioPleer.shared.pauseMusic()
     }
 }
 
@@ -109,11 +146,37 @@ class FinalVC: UIViewController {
         let label = UILabel()
         label.text = "FinalVC"
         label.translatesAutoresizingMaskIntoConstraints = false
+        
+        let playButton = UIButton()
+        playButton.setTitle("PLAY", for: .normal)
+        playButton.addTarget(self, action: #selector(playButtonTapped), for: .touchUpInside)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        let pauseButton = UIButton()
+        pauseButton.setTitle("PAUSE", for: .normal)
+        pauseButton.addTarget(self, action: #selector(pauseButtonTapped), for: .touchUpInside)
+        pauseButton.translatesAutoresizingMaskIntoConstraints = false
+        
         testView.addSubview(label)
+        testView.addSubview(playButton)
+        testView.addSubview(pauseButton)
+        
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: testView.centerYAnchor)
+            label.centerYAnchor.constraint(equalTo: testView.centerYAnchor),
+            playButton.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
+            playButton.topAnchor.constraint(equalTo: label.bottomAnchor),
+            pauseButton.centerXAnchor.constraint(equalTo: testView.centerXAnchor),
+            pauseButton.topAnchor.constraint(equalTo: playButton.bottomAnchor)
         ])
         view = testView
+    }
+    
+    @objc private func playButtonTapped(_ sender: CustomTabBarButton) {
+        AudioPleer.shared.playMusic()
+    }
+    
+    @objc private func pauseButtonTapped(_ sender: CustomTabBarButton) {
+        AudioPleer.shared.pauseMusic()
     }
 }
