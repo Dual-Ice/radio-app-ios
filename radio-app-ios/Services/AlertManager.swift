c914f1f2b7c7878893bc6456f0204df6c9a2c29b
Maksim Stogniy <mstogniy@ya.ru>
2024-08-03 22:15:36 +0400

    Add alerts managers

--- radio-app-ios/Services/AlertManager.swift
+++ radio-app-ios/Services/AlertManager.swift
@@ -5,4 +5,18 @@
 //  Created by  Maksim Stogniy on 03.08.2024.
 //
 
-import Foundation
+import UIKit
+
+class AlertManager {
+    
+    init() {}
+    
+    func presentBasicAlert(
+        on viewController: UIViewController,
+        title: String,
+        message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("AuthScreenUDismissButtonText", comment: ""), style: .default, handler: nil))
+        viewController.present(alert, animated: true)
+    }
+}
