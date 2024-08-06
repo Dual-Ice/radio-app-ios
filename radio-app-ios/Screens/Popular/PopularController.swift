1789d13d90a46655d935ceae219655afcd024964
Valentina Popova <info@valentina-popova.ru>
2024-08-06 12:17:15 +0300

    Add update cell

--- radio-app-ios/Screens/Popular/PopularController.swift
+++ radio-app-ios/Screens/Popular/PopularController.swift
@@ -10,15 +10,21 @@ import UIKit
 final class PopularController: UIViewController {
     
     private let popularView = PopularView()
+    private let apiManager = Radio_API_Manager()
+    private var stations = [Station]() {
+        didSet {
+            popularView.getCollectionView.reloadData()
+        }
+    }
     
-    private var items = [
-        ("POP", "Radio Record", 315, true, UIImage(named: "waveRed")),
-        ("16bit", "Radio Gameplay", 240, false, UIImage(named: "waveBlue")),
-        ("Punk", "Russian Punk rock", 200, false, UIImage(named: "wavePurple")),
-        ("Dj remix", "IREMIX!", 54, false, UIImage(named: "waveGreen")),
-        ("Adult", "RUSSIAN WAVE", 315, false, UIImage(named: "waveYellow")),
-        ("Etnic", "beufm.kz", 74, false, UIImage(named: "waveLightRed"))
-    ]
+//    private var items = [
+//        ("POP", "Radio Record", 315, true, UIImage(named: "waveRed")),
+//        ("16bit", "Radio Gameplay", 240, false, UIImage(named: "waveBlue")),
+//        ("Punk", "Russian Punk rock", 200, false, UIImage(named: "wavePurple")),
+//        ("Dj remix", "IREMIX!", 54, false, UIImage(named: "waveGreen")),
+//        ("Adult", "RUSSIAN WAVE", 315, false, UIImage(named: "waveYellow")),
+//        ("Etnic", "beufm.kz", 74, false, UIImage(named: "waveLightRed"))
+//    ]
     
     // MARK: - Life Cycle
     override func loadView() {
@@ -28,23 +34,36 @@ final class PopularController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         popularView.setDelegates(self, dataSource: self)
-        
-        setupHeaderView()
+        fetchPopularStations()
+//        setupHeaderView()
     }
     
-    private func setupHeaderView() {
-//        popularView.headerView.setTitle("Popular")
+    private func fetchPopularStations() {
+        apiManager.getPopular { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let stations):
+                    self?.stations = stations
+                case .failure(let error):
+                    print("Failed to featch popular stations: \(error)")
+                }
+            }
+        }
     }
+    
+//    private func setupHeaderView() {
+////        popularView.headerView.setTitle("Popular")
+//    }
 }
 
 extension PopularController: PopularViewDelegate {
-    func tappedButton() {
-        print("Button is tapped")
-    }
+//    func tappedButton() {
+//        print("Button is tapped")
+//    }
     
     func cellSelected(at indexPath: IndexPath) {
-        for i in 0..<items.count {
-            items[i].3 = (i == indexPath.row)
+        for i in 0..<stations.count {
+            stations[i].isSelected = (i == indexPath.row)
         }
         popularView.getCollectionView.reloadData()
         print("Selected cell at \(indexPath)")
@@ -54,7 +73,7 @@ extension PopularController: PopularViewDelegate {
 extension PopularController: UICollectionViewDataSource, UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return items.count
+        return stations.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -62,8 +81,14 @@ extension PopularController: UICollectionViewDataSource, UICollectionViewDelegat
             return UICollectionViewCell()
         }
         
-        let item = items[indexPath.row]
-        cell.configure(with: item.0, subtitle: item.1, votes: item.2, isActive: item.3, waveImage: item.4!)
+        let station = stations[indexPath.row]
+        cell.configure(
+            with: station.tags ?? "Unknow Genre",
+            subtitle: station.name ?? "Unknow Station",
+            votes: station.votes ?? 0,
+            isActive: station.isSelected ?? false,
+            waveImage: Image.waveLightRed
+        )
         
         return cell
     }
--- radio-app-ios/Services/Network Layer/Request Models.swift
+++ radio-app-ios/Services/Network Layer/Request Models.swift
@@ -18,6 +18,7 @@ struct Station: Codable {
     let country: String?        /// country the station belongs to, multistring
     let language: String?       /// spoken languages, multistring
     let geo_lat: Double?        /// частота? часто прилетает nil
+    var isSelected: Bool? = false
 }
 
 /// model for getting list of countries
